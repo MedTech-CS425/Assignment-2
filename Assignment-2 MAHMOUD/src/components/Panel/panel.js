edbe4555b33706b064cfe353817bd9aee39d06d8
mahmoudbenhassine <76956098+mahmoudbenhassine@users.noreply.github.com>
2021-06-28 02:26:07 +0100

    Add files via upload

---
+++ Assignment-2 MAHMOUD/src/components/Panel/panel.js
@@ -0,0 +1,21 @@
+import Basket from "../basket/Basket";
+import AddItem from "../addItem/addItem";
+import { useState } from "react";
+import './panel.css'
+
+const Panel=(props)=> {
+    let bucket = props.bucket
+    let categories = props.categories
+    const [addNew, setAddNew] = useState(false);
+    const addNewItem=()=> {
+        setAddNew(true);
+    }
+
+    return (
+        <div >
+            {addNew ? <AddItem categories={categories} cancel={() => setAddNew(false)} /> : <Basket bucket={bucket} addNewItem={addNewItem} />}
+        </div>
+    )
+}
+
+export default Panel;
---
+++ Assignment-2 MAHMOUD/src/components/sideNav/SideNav.js
@@ -0,0 +1,18 @@
+import './SideNav.css'
+
+const SideNav=(props)=> {
+    return (
+        <div className="SideNav">
+            
+            <div className="iconsWrapper">
+                <span className="material-icons">category</span>
+                <span className="material-icons">refresh</span>
+                <span className="material-icons">query_stats</span>
+            </div>
+          
+        </div>
+    )
+}
+
+
+export default SideNav;
